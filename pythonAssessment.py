76c3dc496da75088904b047120021cd519734663
Yasmin101-101 <yasminmohamudyasmin@gmail.com>
2026-02-21 20:25:15 +0300

    Add average word length and paragraph count functions

--- pythonAssessment.py
+++ pythonAssessment.py
@@ -5,6 +5,9 @@ def count_specific_word(text, word ):
     text = text.lower()
     word = word.lower()
     count = len(re.findall(r"\b" + word + r"\b", text))
+    if count == 0:
+        return 0
+
     return count
 
 def identify_most_common_word(text):
@@ -16,10 +19,19 @@ def identify_most_common_word(text):
     most_common = word_count.most_common(1)[0][0]
     return most_common
 
-def calculate_average_word_length(str):
-    pass
-def count_paragraphs(str):
-    pass
+def calculate_average_word_length(text):
+    if not text:
+        return 0
+    words = re.findall(r"\b[a-z]+\b", text.lower())
+    avarage = sum(len(word) for word in words) / len(words) 
+    return float(avarage)
+
+def count_paragraphs(text):
+    if not text :
+        return 1
+    paragraphs = [p for p in text.split("\n\n") if p.strip()]
+    return len(paragraphs)
+    
 def count_sentences(str):
     pass
 
@@ -30,3 +42,9 @@ print(f"The word {search_word} appears {result_word} times in the article.")
 
 most_common_word = identify_most_common_word(text)
 print(f"The most common word: {most_common_word} ")
+
+avarage_lenght = calculate_average_word_length(text)
+print(f"The avarage word lenght is :{avarage_lenght}")
+
+paraghraph_count = count_paragraphs(text)
+print(f"The number of paragraphs:{paraghraph_count}")
